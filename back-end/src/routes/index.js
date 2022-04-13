7417a1a2246e3f07823f7dc2c0daf8d082415fc8
Ashton B <ashtonbrugh@gmail.com>
2022-04-13 15:44:33 -0400

    authentication fixed

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,5 +1,12 @@
 import { testRoute } from './testRoute';
+import { signUpRoute } from './signUpRoute';
+import { loginRoute } from './loginRoute';
+import { updateUserInfoRoute } from './updateUserInfoRoute';
+
 
 export const routes = [
+    loginRoute,
     testRoute,
+    signUpRoute,
+    updateUserInfoRoute,
 ];
