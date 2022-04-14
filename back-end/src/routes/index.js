599afc600c67a1b74f5ea2dc654597f72733d42e
Ashton B <ashtonbrugh@gmail.com>
2022-04-14 11:15:04 -0400

    adding email authentication

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -2,6 +2,7 @@ import { testRoute } from './testRoute';
 import { signUpRoute } from './signUpRoute';
 import { loginRoute } from './loginRoute';
 import { updateUserInfoRoute } from './updateUserInfoRoute';
+import { testEmailRoute } from './testEmailRoute';
 
 
 export const routes = [
